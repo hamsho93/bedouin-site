161817c7b083c6f40dc24989c6083737495d4d54
hamsho93 <hamshom@gmail.com>
2025-01-20 22:21:08 -0500

    fix aws issue v3

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -255,7 +255,7 @@ export const Icons = {
       <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
     </svg>
   ),
-  pill: (props: LucideProps) => (
+  pill2: (props: LucideProps) => (
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
@@ -320,7 +320,7 @@ export const Icons = {
   store: Store,
   user: User,
   brain: Brain,
-  pill: Pill,
+  pill2: Pill,
   stethoscope: Stethoscope,
   analytics: BarChart3,
   shield: ShieldCheck,
