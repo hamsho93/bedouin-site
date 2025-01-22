e0d165e4df6a9de5b3d99adf48bef08956cf2529
hamsho93 <hamshom@gmail.com>
2025-01-21 20:01:36 -0500

    add Heap

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Link from "next/link";
 import { Button } from "@nextui-org/button";
 import { Balancer } from "react-wrap-balancer";
+import Script from 'next/script';
 
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config";
@@ -16,6 +19,7 @@ import { TeamSection } from "@/components/features/team-section";
 export default function Home() {
   return (
     <Shell>
+      <HeapAnalytics />
       {/* Add the Client Component */}
 
       <section className="relative w-full min-h-[800px] flex flex-col items-center justify-center">
@@ -51,5 +55,20 @@ export default function Home() {
       <Dues />
       <TeamSection />
     </Shell>
+  );
+}
+
+export function HeapAnalytics() {
+  return (
+    <Script
+      id="heap-analytics"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+          heap.load("1532518150");
+        `
+      }}
+    />
   );
 }
